300335fffe0cf73499c7c51964df740f210934d8
Christian Lopez <clopez88@depaul.edu>
2022-11-05 18:06:55 -0500

    Added UserOrder + candy counts

--- src/Candy/Candy.sql
+++ src/Candy/Candy.sql
@@ -1,20 +1,21 @@
 CREATE TABLE candy(
     candy_type VARCHAR(20),
-    candy_name VARCHAR(20),
-    candy_is_wrap VARCHAR(3),
     PRIMARY KEY(candy_type)
 );
+--To Test
+INSERT INTO candy(candy_type, candy_is_wrap) VALUES ("Chocolate", "YES")
+INSERT INTO candy(candy_type, candy_is_wrap) VALUES ("Gum", "YES")
+INSERT INTO candy(candy_type, candy_is_wrap) VALUES ("Lollipop", "NO")
+INSERT INTO candy(candy_type, candy_is_wrap) VALUES ("Hard Candy", "YES")
+INSERT INTO candy(candy_type, candy_is_wrap) VALUES ("Sour Candy", "YES")
 
-INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Chocolate", "M&M", "YES")
-INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Gum", "Orbit", "YES")
-INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Lollipop", "DUM DUM", "NO")
-INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Hard Candy", "Jolly Rancher", "YES")
-INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Sour Candy", "Sour Patch Kids", "YES")
-
-
-CREATE TABLE UserCandy(
+--To be used for when a user orders candy.
+CREATE TABLE UserOrder(
     order_id INT PRIMARY KEY,
     order_date DATE,
-    user_sweet VARCHAR(20)
-    FOREIGN KEY(user_sweet) REFERENCES candy()
+    user_sweet VARCHAR(20),
+    buyer VARCHAR(20),
+    FOREIGN KEY(user_sweet) REFERENCES candy(candy_type),
+    FOREIGN KEY(buyer) REFERENCES User_T(user_name)
 );
+INSERT INTO UserOrder(order_id, order_date, user_sweet, buyer) VALUES ("Sour Candy", "Sour Patch Kids", "YES")
--- src/Candy/User.sql
+++ src/Candy/User.sql
@@ -1,9 +1,16 @@
+--Works both both buyer and seller
 CREATE TABLE User_T(
     user_name VARCHAR(20) PRIMARY KEY,
     user_review FLOAT,
     travel_pref VARCHAR(40),
+    choc_count INT,
+    gum_count INT,
+    loli_count INT,
+    hard_count INT,
+    sour_count INT,
 );
 
+--To Test
 INSERT INTO User_T(user_name, user_review, travel_pref) VALUES ("John Smith", 5, "Police Station")
 INSERT INTO User_T(user_name, user_review, travel_pref) VALUES ("Rebecca Joe", 4.5, "Loop Building")
 INSERT INTO User_T(user_name, user_review, travel_pref) VALUES ("Miley Art", 1.2, "White House")
