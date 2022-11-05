b106a0e64b78c862fcfa3e908e512881f7329435
Christian Lopez <clopez88@depaul.edu>
2022-11-05 17:05:36 -0500

    Update User

--- src/Candy/Candy.sql
+++ src/Candy/Candy.sql
@@ -1,19 +1,20 @@
-CREATE TABLE Candy(
+CREATE TABLE candy(
     candy_type VARCHAR(20),
     candy_name VARCHAR(20),
     candy_is_wrap VARCHAR(3),
     PRIMARY KEY(candy_type)
 );
 
-INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Chocolate", "M&M", "YES")
-INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Gum", "Orbit", "YES")
-INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Lollipop", "DUM DUM", "NO")
-INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Hard Candy", "Jolly Rancher", "YES")
-INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Sour Candy", "Sour Patch Kids", "YES")
+INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Chocolate", "M&M", "YES")
+INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Gum", "Orbit", "YES")
+INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Lollipop", "DUM DUM", "NO")
+INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Hard Candy", "Jolly Rancher", "YES")
+INSERT INTO candy(candy_type, candy_name, candy_is_wrap) VALUES ("Sour Candy", "Sour Patch Kids", "YES")
+
 
 CREATE TABLE UserCandy(
-    orderId INT PRIMARY KEY,
-    orderDate DATE,
-
-    
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    user_sweet VARCHAR(20)
+    FOREIGN KEY(user_sweet) REFERENCES candy()
 );
