933379a3e971bef8662c825a58a7c9cca29e04d6
Christian Lopez <clopez88@depaul.edu>
2022-11-05 15:45:02 -0500

    Create User table

--- src/Candy/Candy.sql
+++ src/Candy/Candy.sql
@@ -4,3 +4,9 @@ CREATE TABLE Candy(
     candy_is_wrap VARCHAR(3),
     PRIMARY KEY(candy_type)
 );
+
+CREATE TABLE UserCandy(
+    orderId INT PRIMARY KEY,
+    
+    
+)
---
+++ src/Candy/User.sql
@@ -0,0 +1,5 @@
+CREATE TABLE User(
+    user_name VARCHAR(20) PRIMARY KEY,
+    user_review INT,
+    travel_pref VARCHAR(20),
+);
