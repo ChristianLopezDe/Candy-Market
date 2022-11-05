f385b5c73a78f44fc1dc609a81768472d2db2f6c
Christian Lopez <clopez88@depaul.edu>
2022-11-05 17:06:02 -0500

    Update User

--- src/Candy/User.sql
+++ src/Candy/User.sql
@@ -1,5 +1,9 @@
-CREATE TABLE User(
+CREATE TABLE User_T(
     user_name VARCHAR(20) PRIMARY KEY,
-    user_review INT,
-    travel_pref VARCHAR(20),
+    user_review FLOAT,
+    travel_pref VARCHAR(40),
 );
+
+INSERT INTO User_T(user_name, user_review, travel_pref) VALUES ("John Smith", 5, "Police Station")
+INSERT INTO User_T(user_name, user_review, travel_pref) VALUES ("Rebecca Joe", 4.5, "Loop Building")
+INSERT INTO User_T(user_name, user_review, travel_pref) VALUES ("Miley Art", 1.2, "White House")
