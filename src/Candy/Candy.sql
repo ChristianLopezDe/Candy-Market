a9206d32415ee71e043b9292db1ceeca92d8063b
Christian Lopez <clopez88@depaul.edu>
2022-11-05 16:33:31 -0500

    Insert examples

--- src/Candy/Candy.sql
+++ src/Candy/Candy.sql
@@ -5,8 +5,15 @@ CREATE TABLE Candy(
     PRIMARY KEY(candy_type)
 );
 
+INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Chocolate", "M&M", "YES")
+INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Gum", "Orbit", "YES")
+INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Lollipop", "DUM DUM", "NO")
+INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Hard Candy", "Jolly Rancher", "YES")
+INSERT INTO student(candy_type, candy_name, candy_is_wrap) VALUES ("Sour Candy", "Sour Patch Kids", "YES")
+
 CREATE TABLE UserCandy(
     orderId INT PRIMARY KEY,
+    orderDate DATE,
+
     
-    
-)
+);
