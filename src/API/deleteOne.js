9308a7170972f23268f0afd309581c438c2c46c0
RaptorCentauri <dave.leo.shilander@icloud.com>
2022-11-05 16:18:27 -0500

    restarting

---
+++ src/API/deleteOne.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const deleteOne = async () => {
+    let url = ``;
+    let objBody = {}
+    let res = await axios.delete(url, objBody)
+return res
+
+}
+
+export default deleteOne;
---
+++ src/API/post.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const post = async () => {
+    let url = ``;
+    let objBody = {}
+    let res = await axios.post(url, objBody)
+return res
+
+}
+
+export default post;
